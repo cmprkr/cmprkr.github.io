5b045e53b7bd83680a159bf17068bb517e3c3b32
cam <121907837+cmprkr@users.noreply.github.com>
2023-03-21 09:06:06 -0400

    just see where this goes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-document.addEventListener('click', function(event) {
-    
-    if (event.target.matches('.no-html-extension')) {
-        event.preventDefault();
-        history.pushState(null, null, event.target.href);
-    };
-});
-
 document.addEventListener("DOMContentLoaded", function() {
 
     function clearMainContent() {
